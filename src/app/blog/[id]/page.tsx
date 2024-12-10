0b3812d8811a6c785d769eedde3f99ab7ff63b4a
Aphiwit1 <aphiwit@codework-tech.com>
2024-12-10 11:57:14 +0700

    mock isr

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,18 +1,3 @@
-interface Post {
-  id: string;
-  title: string;
-  content: string;
-}
-
-// // Next.js will invalidate the cache when a
-// // request comes in, at most once every 60 seconds.
-// export const revalidate = 60;
-
-// // We'll prerender only the params from `generateStaticParams` at build time.
-// // If a request comes in for a path that hasn't been generated,
-// // Next.js will server-render the page on-demand.
-// export const dynamicParams = true; // or false, to 404 on unknown paths
-
 export async function generateStaticParams() {
   const ids = ["1", "2", "3"];
   return ids.map((id) => {
@@ -22,7 +7,7 @@ export async function generateStaticParams() {
   });
 }
 
-export default async function Page({ params }: { params: { id: string } }) {
+export default async function Page({ params }: any) {
   return (
     <main>
       <h1>{params.id}</h1>
